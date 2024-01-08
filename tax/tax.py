9ba617945ad9f9179ea4d68593316d5ea8979333
panxiaoyu <panxiaoyu@cmhi.chinamobile.com>
2024-01-08 11:42:08 +0800

    [MOD] normalize the code

--- tax/tax.py
+++ tax/tax.py
@@ -1,70 +1,66 @@
-
 import sys
 
 
-def calc_tax(saForTax):
-    if saForTax <=0:
+def calc_tax(salary_without_deduction):
+    if salary_without_deduction <=0:
         tax=0
-    elif saForTax <= 36000:
-        tax = saForTax * 0.03
-    elif saForTax <= 144000:
-        tax = saForTax * 0.1 - 2520
-    elif saForTax <= 300000:
-        tax = saForTax * 0.2 - 16920
-    elif saForTax <= 420000:
-        tax = saForTax * 0.25 - 31920
-    elif saForTax <= 660000:
-        tax = saForTax * 0.3 - 52920
-    elif saForTax <= 960000:
-        tax = saForTax * 0.35 - 85920
+    elif salary_without_deduction <= 36000:
+        tax = salary_without_deduction * 0.03
+    elif salary_without_deduction <= 144000:
+        tax = salary_without_deduction * 0.1 - 2520
+    elif salary_without_deduction <= 300000:
+        tax = salary_without_deduction * 0.2 - 16920
+    elif salary_without_deduction <= 420000:
+        tax = salary_without_deduction * 0.25 - 31920
+    elif salary_without_deduction <= 660000:
+        tax = salary_without_deduction * 0.3 - 52920
+    elif salary_without_deduction <= 960000:
+        tax = salary_without_deduction * 0.35 - 85920
     else:
-        tax = saForTax * 0.45 - 181920
+        tax = salary_without_deduction * 0.45 - 181920
 
     return tax
 
 if __name__ == "__main__":
-    sa = int(input("请输入月工资："))
-    # if sa >= 25000:
-    #     print("妈的工资这么高？拜拜")
-    #     sys.exit(0)
+    salary = int(input("请输入月工资："))
+    social_security = int(input("请输入社保基数："))
+    social_security = social_security * 0.103
+    housing_fund = int(input("请输入公积金基数："))
+    housing_fund = housing_fund * 0.12
+    deductioin = int(input("请输入月度专项扣除金额："))
+    annual_bonus = int(input("请输入年终奖系数："))
+    annual_bonus = annual_bonus * salary
 
-    sb = int(input("请输入社保基数："))
-    sb = sb * 10.5 * 0.01
-    gjj = int(input("请输入公积金基数："))
-    gjj = gjj * 0.12
-    kou = int(input("请输入月度专项扣除金额："))
-    bonus = int(input("请输入年终奖金额："))
-
-    sumSa = 0
-    sumK = 0
-    sumGet = 0
-    sumTax = 0
+    sum_of_salary = 0
+    sum_of_deduction = 0
+    sum_of_money_got = 0
+    sum_of_tax = 0
     print("月份\t当月交税\t当月实发\\累计工资")
 
     # calculate tax for salary of every month
     for i in range(12):
-        sumSa += sa
-        sumK += sb+gjj+kou+5000
-        saForTax = sumSa - sumK
+        sum_of_salary += salary
+        sum_of_deduction += social_security + housing_fund + deductioin + 5000  # 5000 for base salary no need to pay tax each month
+        salary_without_deduction = sum_of_salary - sum_of_deduction
         
-        tax = calc_tax(saForTax)
+        tax = calc_tax(salary_without_deduction)
         
-        tax = tax - sumTax
-        sumTax += tax
-        saGet = sa - tax - sb - gjj
-        sumGet += saGet
+        tax = tax - sum_of_tax
+        sum_of_tax += tax
+        salary_got = salary - tax - social_security - housing_fund
+        sum_of_money_got += salary_got
         
         print("{0}月：\t".format(i+1),end="")
-        print('{0}\t\t{1}/{2}'.format(round(tax), round(saGet), round(sumGet)))
+        print('{0}\t\t{1}/{2}/{3}'.format(round(tax), round(salary_got), round(2*housing_fund), round(sum_of_money_got)))
 
-    # calculate tax for bonus
-    tax_for_bonus = calc_tax(saForTax + bonus) - sumTax
-    bonus_could_get = bonus - tax_for_bonus
+    # calculate tax for annual_bonus
+    tax_for_bonus = calc_tax(salary_without_deduction + annual_bonus) - sum_of_tax
+    bonus_could_get = annual_bonus - tax_for_bonus
     print("年终奖：{0}\t\t{1}".format(round(tax_for_bonus), round(bonus_could_get)))
     print("=================================================")
-    print('月工资: {0}'.format(round(sumGet/12)))
-    print('月工资+公积金: {0}'.format(gjj*2+round(sumGet/12)))
-    print('年工资: {0}'.format(sumGet+gjj*2*12+bonus_could_get))
-    print('年缴税: {0}'.format(tax_for_bonus+sumTax))
+    print('月工资: {0}'.format(round(sum_of_money_got/12)))
+    print('月工资+公积金: {0}'.format(housing_fund * 2 + round(sum_of_money_got/12)))
+    print('年工资: {0}'.format(sum_of_money_got + housing_fund*2*12 + bonus_could_get))
+    print('年缴税: {0}'.format(tax_for_bonus + sum_of_tax))
 
     input("press any key to exit...")
